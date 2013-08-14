2c6952b617f23b686b7c058d2f39ac85fadb5ed7
andres1905 <andres1905@gmail.com>
2013-08-14 11:39:26 -0500

    [Carlos] Changed for loops to each blocks

--- lib/book_sets_manager.rb
+++ lib/book_sets_manager.rb
@@ -15,7 +15,7 @@ class BookSetsManager
   def discover_set_in_books books
     book_set = books.uniq
     remaining_books = Array.new(books)
-    for book in book_set
+    book_set.each do |book|
       remaining_books.delete_at(remaining_books.index(book));
     end
     @book_sets << book_set
@@ -26,13 +26,13 @@ class BookSetsManager
   
   def balance_book_sets
     average_size = get_book_sets_average_size
-    for book_set in @book_sets
-      for book_set_2 in @book_sets
-        if can_balance_book_sets?(book_set,book_set_2,average_size)
+    @book_sets.each do |book_set|
+      @book_sets.each do |book_set_2|
+        if could_balance_book_sets?(book_set,book_set_2,average_size)
           books_difference = book_set - book_set_2
-          if (books_difference.length > 0)
+          if !books_difference.empty?
             book = books_difference.pop
-            book_set_2.push(book)
+            book_set_2 << book
             book_set.delete(book)
           end
         end
@@ -42,13 +42,13 @@ class BookSetsManager
   
   def get_book_sets_average_size
     average_size = 0
-    for book_set in @book_sets
+    @book_sets.each do |book_set|
       average_size += book_set.length
     end
     (average_size / @book_sets.length).round(0)
   end
   
-  def can_balance_book_sets?(book_set, book_set_2, average_size)
-    book_set_2.length < average_size && book_set != book_set_2 && book_set.length > book_set_2.length
+  def could_balance_book_sets?(book_set, book_set_2, average_size)
+    book_set != book_set_2 && book_set_2.length < average_size && book_set.length > book_set_2.length
   end
 end
